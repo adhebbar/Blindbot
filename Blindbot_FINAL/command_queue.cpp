1dd6dcba4cc032437fc8474499094161a5bcd18d
Adolfo-Karim <adolfokarim@gmail.com>
2017-04-12 03:00:53 -0400

    Added command queue interface

--- Blindbot_FINAL/command_queue.cpp
+++ Blindbot_FINAL/command_queue.cpp
@@ -8,5 +8,33 @@ C_Queue::C_Queue(int capacity){
 	back = 0;
 }
 
+int queue_size(){
+	return size;
+}
 
+int queue_capacity(){
+	return capacity;
+}
 
+void enq(String elem){
+	if (size == capacity) return;
+	if (size == 0){
+		front = 0;
+		back = 1;
+		command_q[front] = elem;
+	}
+	command_q[back&capacity] = elem;
+	back = (back + 1)%capacity;
+}
+
+String deq(){
+	if (size == 0) return;
+	String elem = command_q[front];
+	front = (front + 1) % capacity;
+	return elem;
+}
+
+String queeu_peek(int index){
+	if (index > size) return;
+	return command_q[(front+i)% capacity];
+}
