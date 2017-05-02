0964f500fbb6437b77848e643fd164730a79a57a
Adolfo-Karim <adolfokarim@gmail.com>
2017-05-02 19:18:07 -0400

    Improvements in style

--- Blindbot_FINAL/utility_functions.h
+++ Blindbot_FINAL/utility_functions.h
@@ -15,8 +15,8 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
 void print_val(String val_name, float val);
 bool check_valid_pos(float x, float y, float z); //NEEDS TO BE IMPLEMENTED
 int signed_area(float x[], float y[], int n);
-void centroid_polygon(float* cx, float* cy,float x_coords[], float y_coords[], int legs);
-void centroid_triangle(float* cx, float* cy, float x_coords[], float y_coords[], int legs);
+void centroid_polygon(float &cx, float &cy,float x_coords[], float y_coords[], int legs);
+void centroid_triangle(float &cx, float &cy, float x_coords[], float y_coords[], int legs);
 float convert_to_kg(float forces[],int num_legs);
 float convert_force(int force);
 void get_command(char command[])
